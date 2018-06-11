7c8d3ae0cf77f544aa840d84d6992445f1961c27
USER <USER@192.168.0.9>
2018-06-11 15:08:18 +0530

    dev3

--- TL/GITHubDemo/src/com/nt/github/FirstApp.java
+++ TL/GITHubDemo/src/com/nt/github/FirstApp.java
@@ -3,8 +3,8 @@ package com.nt.github;
 public class FirstApp {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		
-		System.out.println("wel come to git_hub");
 		
 	}
 
