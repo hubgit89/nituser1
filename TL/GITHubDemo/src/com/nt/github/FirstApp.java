db52aa16dc79dc68ea6d5bffe6a1e148eb508c5b
USER <USER@192.168.0.9>
2018-06-11 15:40:46 +0530

    Dev10

--- TL/GITHubDemo/src/com/nt/github/FirstApp.java
+++ TL/GITHubDemo/src/com/nt/github/FirstApp.java
@@ -4,13 +4,11 @@ public class FirstApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-<<<<<<< HEAD
-	
-=======
+
 		System.out.println("Hello bhai");
 		
 		
->>>>>>> branch 'master' of https://github.com/hubgit89/nituser1.git
+//>>>>>>> branch 'master' of https://github.com/hubgit89/nituser1.git
 		
 	}
 
